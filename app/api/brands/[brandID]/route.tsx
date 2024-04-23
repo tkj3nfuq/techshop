8819e8e3444c3660e68cf9cd7afe6231d1918e80
tkj3nfuq <thinphcv5@gmail.com>
2024-04-23 19:13:07 +0700

    api bug fixed

--- app/api/brands/[brandID]/route.tsx
+++ app/api/brands/[brandID]/route.tsx
@@ -17,7 +17,7 @@ export async function GET(req: Request) {
 export async function PATCH(req: Response) {
     const brandID = new URL(req.url).pathname.split("/").pop();
 
-    const { name, description, email, phoneNumber } = await req.json();
+    const { name, description, email, phoneNumber, image } = await req.json();
 
     if (!brandID) {
         throw new Error('Brand ID is missing in the request body.');
@@ -32,6 +32,7 @@ export async function PATCH(req: Response) {
             description,
             email,
             phoneNumber,
+            image: image || undefined
         }
     });
     return NextResponse.json(updatedBrand)
